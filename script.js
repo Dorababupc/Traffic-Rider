87c35794945aca91403f63942a1e5c5618df127d
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-06-18 22:11:00 +0530

    Event listener to keys
    For both pressing down and releasing up we have added event listeners to the keys -may be left , right, top, bottom arrows or other keys. We have  disabled their default behaviour and just consoled the keys.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const score=document.querySelector('.score');
 const startScreen=document.querySelector('.startScreen');
 const gameArea=document.querySelector('.gameArea');
-console.log(score);
-console.log(startScreen);
-console.log(gameArea);
+document.addEventListener('keyup',keyUp);
+document.addEventListener('keydown',keyDown);
+function keyUp(e){
+    e.preventDefault();
+    console.log(`${e.key} releasing`);
+}
+function keyDown(e){
+    e.preventDefault();
+    console.log(`${e.key} pressing`);
+}
