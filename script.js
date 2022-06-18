d22f4280fa546a03b85cea0ed712c2993a638141
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-06-18 22:21:55 +0530

    identifying the pressed keys
    Here we have created an object of all 4 keys to identify which key user is pressing and are updating the key as true or false, accordingly.

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const startScreen=document.querySelector('.startScreen');
 const gameArea=document.querySelector('.gameArea');
 document.addEventListener('keyup',keyUp);
 document.addEventListener('keydown',keyDown);
+const keys={
+    ArrowUp:false,
+    ArrowDown:false,
+    ArrowLeft:false,
+    ArrowRight:false
+}
 function keyUp(e){
     e.preventDefault();
-    console.log(`${e.key} releasing`);
+    // console.log(`${e.key} releasing`);
+    keys[e.key]=false;
+    console.log(keys);
 }
 function keyDown(e){
     e.preventDefault();
-    console.log(`${e.key} pressing`);
+    // console.log(`${e.key} pressing`);
+    keys[e.key]=true;
+    console.log(keys);
 }
